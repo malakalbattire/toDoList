3f91700712a54f5c918c45eb95657f94c1eecc28
malakalbattire <malak.albattire@gmail.com>
2024-02-01 19:43:10 +0200

    toggle

--- main.js
+++ main.js
@@ -2,25 +2,23 @@ let addBtn = document.querySelector('button');
 addBtn.onclick = addTask;
 
 function addTask() {
-  let taskInput = document.querySelector('input');
-  let taskValue = taskInput.value;
+  const taskInput = document.querySelector('input');
+  const taskValue = taskInput.value;
 
-  var li = document.createElement('li');
-  var checkbox = document.createElement('input');
+  const li = document.createElement('li');
+  const checkbox = document.createElement('input');
   checkbox.type = 'checkbox';
   li.appendChild(checkbox);
 
-  var textNode = document.createTextNode(taskValue);
+  const textNode = document.createTextNode(taskValue);
   li.appendChild(textNode);
 
-  var ul = document.getElementById('taskList');
+  const ul = document.getElementById('taskList');
   ul.appendChild(li);
 
   taskInput.value = '';
 
-  checkbox.onclick = checked;
-  function checked() {
-    li.style.textDecoration = 'line-through';
-    li.style.background = 'rgb(213, 236, 213)';
+  checkbox.onclick = function() {
+    li.classList.toggle('cheked');
   }
 }
